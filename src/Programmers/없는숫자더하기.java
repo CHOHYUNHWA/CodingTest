0808a3188483d133b0c8d4dc33ad82d7639a4b1c
CHOHYUNHWA <gusk115@naver.com>
2022-06-22 16:24:42 +0900

    2022.6.22

--- src/Programmers/없는숫자더하기.java
+++ src/Programmers/없는숫자더하기.java
@@ -1,11 +1,11 @@
-package 없는숫자더하기;
+package Programmers;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
-
-class Solution {
-    public int solution(int[] numbers) {
+class 없는숫자더하기 {
+    public static void main(String[] args) {
+        int[] numbers = {4,5};
+        System.out.println(solution(numbers));
+    }
+    public static int solution(int[] numbers) {
         int[] a = {1,2,3,4,5,6,7,8,9,0};
         int answer = 45;
         for(int i = 0; i<10; i++){
