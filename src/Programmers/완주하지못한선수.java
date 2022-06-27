ce687eab3e88fa55a92c33dcbaf6ced38e7f1d1a
CHOHYUNHWA <gusk115@naver.com>
2022-06-27 23:16:38 +0900

    2022.6.27

---
+++ src/Programmers/완주하지못한선수.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+class Solution {
+    public static String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for (int i=0; i<completion.length; i++){
+            if (!participant[i].equals(completion[i])){
+                return participant[i];
+            }
+        }
+        return participant[participant.length-1];
+    }
+}
