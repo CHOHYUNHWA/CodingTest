7160484b2248e5376b794368232da7b2dc1b2bfb
CHOHYUNHWA <gusk11@naver.com>
2022-07-01 00:13:00 +0900

    2022.7.1

---
+++ src/Programmers/음양더하기.java
@@ -0,0 +1,24 @@
+package Programmers;
+
+public class 음양더하기 {
+    public static void main(String[] args) {
+        int[] absolutes = {3,6,7,8};
+        boolean[] signs = {true,true,false,true};
+    }
+    int answer = 0;
+    public int solution(int[] absolutes, boolean[] signs) {
+        int[] tmp = new int[signs.length];
+        for(int i=0; i<absolutes.length; i++){
+            if(signs[i] == true){
+                tmp[i] = absolutes[i];
+            } else {
+                tmp[i] = -absolutes[i];
+            }
+        }
+        for(int i=0; i<absolutes.length; i++){
+            answer = answer + tmp[i];
+        }
+
+        return answer;
+    }
+}
