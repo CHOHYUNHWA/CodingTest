9ca49e4bbb8b7ceedd0a3586220fe78b31815b52
CHOHYUNHWA <gusghk115@gmail.com>
2022-07-17 19:37:49 +0900

    2022-07-17

---
+++ src/Programmers/문자열다루기기본.java
@@ -0,0 +1,21 @@
+package Programmers;
+
+class 문자열다루기기본 {
+    public static void main(String[] args) {
+        String s = "abcd";
+        String s1 = "12345";
+        System.out.println(solution(s1));
+    }
+
+
+    public static boolean solution(String s) {
+        boolean answer = true;
+        if(s.length() == 4 || s.length() ==6) {
+            if (s.chars()
+                    .allMatch(ch -> Character.isDigit(ch))) {
+                return answer;
+            }
+        }
+        return false;
+    }
+}
