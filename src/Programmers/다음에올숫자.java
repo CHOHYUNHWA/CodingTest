0b43da8bc5193320fb4fbf5f604a751c84d68c92
CHOHYUNHWA <gusghk115@gmail.com>
2022-11-03 16:19:27 +0900

    다음에올숫자

---
+++ src/Programmers/다음에올숫자.java
@@ -0,0 +1,20 @@
+package Programmers;
+
+public class 다음에올숫자 {
+    public static void main(String[] args) {
+        long[] common = {0, 2, 4};
+        System.out.println(solution(common));
+    }
+
+    public static long solution(long[] common) {
+        long answer = 0;
+        if(common[1] - common[0] == common[2] - common[1]){
+            answer = common[common.length-1] + (common[1]-common[0]);
+        }
+       else{
+            answer = common[common.length-1] * (common[1] / common[0]);
+        }
+        return answer;
+    }
+}
+
