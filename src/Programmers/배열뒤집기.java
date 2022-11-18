62d6063e4b03efd94b53b51c435e6f01e7dc1009
CHOHYUNHWA <gusghk115@gmail.com>
2022-11-18 16:51:11 +0900

    2022.11.18

---
+++ src/Programmers/배열뒤집기.java
@@ -0,0 +1,18 @@
+package Programmers;
+
+import java.util.ArrayList;
+
+public class 배열뒤집기 {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i = 0; i < num_list.length; i++) {
+            list.add(num_list[num_list.length-1-i]);
+        }
+        for(int i =0; i<list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
+
---
+++ src/Programmers/분수의덧셈_미해결.java
@@ -0,0 +1,35 @@
+package Programmers;
+
+import java.util.Arrays;
+
+public class 분수의덧셈_미해결 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(10,2,1,3)));
+    }
+
+    public static int[] solution(int denum1, int num1, int denum2, int num2) {
+        int[] answer = new int[2];
+        int denum3 = denum1*num2 + denum2*num1;
+        int num3 = num1*num2;
+        int gcd =0;
+        if(denum3%num3 ==0){
+            answer[0] = denum3/num3;
+            answer[1] = 1;
+        }
+        if(num3%(denum3%num3) == 0){
+            gcd = denum3%num3;
+            answer[0] = denum3/gcd;
+            answer[1] = num3/gcd;
+        } else{
+            answer[0] = denum3;
+            answer[1] = num3;
+        }
+        return answer;
+    }
+}
+
+//8/10 경우
+//최소공약수를 구해야함.
+//유클리드호제법으로 (60,48의 경우)
+// 60%48 = 12
+// 48%12 =0  a,b 의나머지(c)로 b를 나눴을때 0 이면 최소공약수가됨.
