d249e7df1b847a3f91e8b0c9e4097e92e40dd8ec
CHOHYUNHWA <gusghk115@gmail.com>
2022-12-06 17:09:37 +0900

    2022_12_06

---
+++ src/Programmers/숨어있는숫자의덧셈_투.java
@@ -0,0 +1,38 @@
+package Programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class 숨어있는숫자의덧셈_투 {
+    public static void main(String[] args) {
+        System.out.println(solution("aAb1B2cC34oOp"));
+
+    }
+    public static int solution(String my_string) {
+        int answer = 0;
+        String[] arr = my_string.split("[a-zA-z]");
+        for(int i = 0; i < arr.length; i++){
+//            if(arr[i].contains("1") ||
+//                    arr[i].contains("2") ||
+//                    arr[i].contains("3") ||
+//                    arr[i].contains("4") ||
+//                    arr[i].contains("5") ||
+//                    arr[i].contains("6") ||
+//                    arr[i].contains("7") ||
+//                    arr[i].contains("8") ||
+//                    arr[i].contains("9") ||
+//                    arr[i].contains("0")){
+            if(!arr[i].equals("")){
+                answer += Integer.parseInt(arr[i]);
+            }
+        }
+//        String str = my_string.replaceAll("[a-zA-Z]"," ");
+//        System.out.println(str);
+//        String[] arr = str.split(" ");
+//        System.out.println(Arrays.toString(arr));
+//        String [] str = my_string.replaceAll("[a-zA-Z]"," ").split("");
+        return answer;
+    }
+}
--- src/Programmers/외계어사전.java
+++ src/Programmers/외계어사전.java
@@ -1,28 +1,26 @@
 package Programmers;
 
+import java.util.Arrays;
+
 public class 외계어사전 {
     public static void main(String[] args) {
-        System.out.println(solution(new String[]{"p","o","s"},new String[]{"sod", "eocd", "qixm", "adio", "soo"}));
+        System.out.println(solution(new String[]{"z","d","x"},new String[]{"def", "dww", "dzx"}));
         }
     public static int solution(String[] spell, String[] dic) {
         int answer = 2;
-        int[] answerArr = new int[spell.length];
-        int count = 0;
+        String s = "";
+        Arrays.sort(spell);
+        for(int i = 0; i < spell.length; i++) {
+            s += spell[i];
+        }
         for(int i = 0; i < dic.length; i++) {
-            for(int l = 0; l < answerArr.length; l++) {
-                if(count == answerArr.length){
-                    return 1;
-                }
-                if(answerArr[l] == 1){
-                    count++;
-                }
-            }
-            for(int j = 0; j < spell.length; j++) {
-                for(int k = 0; k < dic[j].length(); k++){
-                    if(dic[i].charAt(j) == spell[k].charAt(k)){
-                        answerArr[k]++;
-                    }
-                }
+            char[] chars = dic[i].toCharArray();
+            Arrays.sort(chars);
+            dic[i] = new String(chars);
+        }
+        for(int i = 0; i < dic.length; i++) {
+            if(dic[i].equals(s)){
+                return 1;
             }
         }
         return answer;
