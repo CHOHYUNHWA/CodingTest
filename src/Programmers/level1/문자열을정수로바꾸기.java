323788cd62446f83f44c1d0e2c1a1a02112b5200
CHOHYUNHWA <gusghk115@gmail.com>
2023-03-28 23:03:40 +0900

    2023_03_28

---
+++ src/Programmers/level1/문자열을정수로바꾸기.java
@@ -0,0 +1,9 @@
+package Programmers.level1;
+
+public class 문자열을정수로바꾸기 {
+    public static int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
