286533456a83a568de52555f80582c4ee0a2001d
CHOHYUNHWA <gusghk115@gmail.com>
2023-04-04 09:25:48 +0900

    2023_04_04

---
+++ src/Programmers/level1/문자열내림차순으로배치하기.java
@@ -0,0 +1,17 @@
+package Programmers.level1;
+
+import java.util.Arrays;
+
+public class 문자열내림차순으로배치하기 {
+    public static void main(String[] args) {
+        System.out.println(solution("Zbcdefg").toString());
+    }
+    public static String solution(String s) {
+        String answer = "";
+        char[] answerList = s.toCharArray();
+        Arrays.sort(answerList);
+        answer = answerList.toString();
+        return answer;
+    }
+}
+
