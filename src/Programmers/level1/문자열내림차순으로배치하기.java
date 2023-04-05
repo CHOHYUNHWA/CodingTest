dc6654ad04a7314e88c36cac759b7d20223fb7b2
CHOHYUNHWA <gusghk115@gmail.com>
2023-04-05 23:05:48 +0900

    2023_04_05

--- src/Programmers/level1/문자열내림차순으로배치하기.java
+++ src/Programmers/level1/문자열내림차순으로배치하기.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class 문자열내림차순으로배치하기 {
     public static void main(String[] args) {
-        System.out.println(solution("Zbcdefg").toString());
+        System.out.println(solution("Zbcdefg"));
     }
     public static String solution(String s) {
         String answer = "";
---
+++ src/Programmers/level1/약수의개수와덧셈.java
@@ -0,0 +1,14 @@
+package Programmers.level1;
+
+public class 약수의개수와덧셈 {
+    public static int solution(int left, int right) {
+        int answer = 0;
+        for(int i = left; i<=right; left ++){
+            for(int j=1; j<=left; j++){
+                if()
+            }
+        }
+        return answer;
+    }
+}
+
