e940bfa4fabde23412d92ab09614732890455697
CHOHYUNHWA <gusghk115@gmail.com>
2023-04-06 14:46:40 +0900

    2023_04_06

--- src/Programmers/level1/약수의개수와덧셈.java
+++ src/Programmers/level1/약수의개수와덧셈.java
@@ -1,12 +1,21 @@
 package Programmers.level1;
 
 public class 약수의개수와덧셈 {
+    public static void main(String[] args) {
+        System.out.println(solution(13,17));
+    }
     public static int solution(int left, int right) {
         int answer = 0;
-        for(int i = left; i<=right; left ++){
+        for(int i = left; left<=right; left++){
+            int tmp = 0;
             for(int j=1; j<=left; j++){
-                if()
+                if(left%j == 0){
+                    tmp++;
+                }
             }
+            if(tmp%2 == 0){
+                answer += left;
+            }else {answer -= left;}
         }
         return answer;
     }
